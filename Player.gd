ead5c96f5a3ec5ddcc6c2adb42b9f02636fa4cc5
bdunko <bdunko@gmail.com>
2023-03-25 15:42:19 -0400

    added acceleration/friction

--- Player.gd
+++ Player.gd
@@ -1,11 +1,21 @@
 extends CharacterBody2D
 
-@export var speed = 400
+@export var speed = 200
+@export var acceleration = 100000 
+@export var friction = 100000
 
-func get_input():
+func _ready():
+	assert(speed > 0)
+	assert(acceleration > 0)
+	assert(friction > 0)
+
+func update_velocity(delta):
 	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * speed
+	velocity += input_direction * acceleration * delta
+	velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+	velocity = velocity.clamp(Vector2(-speed, -speed), Vector2(speed, speed))
+	
 
 func _physics_process(delta):
-	get_input()
+	update_velocity(delta)
 	move_and_slide()
