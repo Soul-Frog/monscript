636277b577c3bc09583a9ccfc89e4e40787bf5ca
bdunko <bdunko@gmail.com>
2023-03-25 16:08:05 -0400

    added collision to tilemap

--- Player.gd
+++ Player.gd
@@ -1,19 +1,13 @@
 extends CharacterBody2D
 
 @export var speed = 200
-@export var acceleration = 100000 
-@export var friction = 100000
 
 func _ready():
 	assert(speed > 0)
-	assert(acceleration > 0)
-	assert(friction > 0)
 
 func update_velocity(delta):
 	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity += input_direction * acceleration * delta
-	velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
-	velocity = velocity.clamp(Vector2(-speed, -speed), Vector2(speed, speed))
+	velocity = speed * input_direction
 	
 
 func _physics_process(delta):
