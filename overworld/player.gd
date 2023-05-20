d3fb8ac6569037aa61d4b479366647987b848214
DESKTOP-EMAAN\emaanr <em99nr@gmail.com>
2023-05-19 21:16:25 -0400

    Initial appraoch for dashing

--- overworld/player.gd
+++ overworld/player.gd
@@ -3,8 +3,21 @@ extends CharacterBody2D
 const SPEED = 200
 const INVINCIBILITY_AFTER_ESCAPE_SECS = 5
 const INVINCIBILITY_AFTER_WIN_SECS = 2
+const DASH_DURATION = 0.05
+const DASH_SPEED = SPEED * 4
+
 var is_invincible = false
 var escaped_recently = false
+
+var orientation = Vector2.ZERO
+var dashing = false
+
+func dash():
+	if Input.is_action_just_pressed("dash"):
+		dashing = true
+		await get_tree().create_timer(DASH_DURATION).timeout
+		dashing = false
+		print("DASH")
 
 func _ready():
 	assert(SPEED > 0)
@@ -12,8 +25,13 @@ func _ready():
 func update_velocity(_delta):
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = SPEED * input_direction
+	if dashing:
+		velocity = DASH_SPEED * orientation
+	if input_direction != Vector2.ZERO and not dashing:
+		orientation = input_direction
 
 func _physics_process(delta):
+	dash()
 	update_velocity(delta)
 	move_and_slide()
 
@@ -23,7 +41,6 @@ func _on_area_2d_body_entered(overworld_encounter_collided_with):
 		Events.emit_signal("battle_started", overworld_encounter_collided_with.mons)
 
 func activate_invincibility(battle_end_condition):
-	# assert that you went from battle scene -> overworld scene before this function is called?
 	is_invincible = true
 	if battle_end_condition == Global.BattleEndCondition.ESCAPE:
 		Global.call_after_delay(INVINCIBILITY_AFTER_ESCAPE_SECS, func(): is_invincible = false)
