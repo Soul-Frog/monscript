252bf1c65f1a2c23bb84c58799bf91e5aa85b468
bdunko <bdunko@gmail.com>
2023-06-22 21:26:55 -0400

    Added to bar to script menu

--- ui/script/script_menu.gd
+++ ui/script/script_menu.gd
@@ -1,5 +1,5 @@
 class_name ScriptUI
-extends ScrollContainer
+extends VBoxContainer
 
 func _ready() -> void:
 	_add_new_line()
@@ -8,15 +8,15 @@ func _add_new_line() -> void:
 	var line: ScriptLine = load("res://ui/script/script_line.tscn").instantiate()
 	line.line_started.connect(_on_line_started)
 	line.line_deleted.connect(_on_line_deleted)
-	$Lines.add_child(line)
+	$Window/Lines.add_child(line)
 	
 func _on_line_started() -> void:
 	_add_new_line()
 
 func _on_line_deleted(line: ScriptLine) -> void:
 	line.queue_free()
-	$Lines.remove_child(line)
-	if $Lines.get_child_count() == 0:
+	$Window/Lines.remove_child(line)
+	if $Window/Lines.get_child_count() == 0:
 		_add_new_line()
 
 func export_script() -> void:
