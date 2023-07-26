a868608cc41f0076fe205b0e24ff8458ef618c25
bdunko <bdunko@gmail.com>
2023-07-25 21:23:24 -0400

    Added custom scrollbar to database ui

--- ui/database/database_menu.gd
+++ ui/database/database_menu.gd
@@ -1,5 +1,5 @@
 class_name UIDatabaseMenu
-extends VBoxContainer
+extends Node2D
 
 # emitted when this menu should be closed
 signal closed
@@ -12,13 +12,14 @@ const DATABASE_ENTRY_SCENE := preload("res://ui/database/database_entry.tscn")
 func _ready() -> void:
 	# create the database entries, one for each mon type
 	for montype in MonData.MonType.values():
-		if montype == MonData.MonType.NONE:
-			continue
-		var entry: DatabaseEntry = DATABASE_ENTRY_SCENE.instantiate()
-		entry.set_texture(MonData.get_texture_for_mon(montype))
-		entry.set_progress(0)
-		_montype_to_entry_map[montype] = entry
-		$Contents/DatabaseScroll/Database.add_child(entry)
+		for i in range(0, 10): # TODO - remove this
+			if montype == MonData.MonType.NONE:
+				continue
+			var entry: DatabaseEntry = DATABASE_ENTRY_SCENE.instantiate()
+			entry.set_texture(MonData.get_texture_for_mon(montype))
+			entry.set_progress(0)
+			_montype_to_entry_map[montype] = entry
+			$DatabaseScroll/Database.add_child(entry)
 
 func setup() -> void:
 	# update progress values for each mon
