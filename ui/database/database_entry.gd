fedacf1f2c0a719724569c5fb5de9e7fca56d1f1
bdunko <bdunko@gmail.com>
2023-07-27 21:15:32 -0400

    Database entry graphics completed; clickable database entries

--- ui/database/database_entry.gd
+++ ui/database/database_entry.gd
@@ -1,10 +1,34 @@
 class_name DatabaseEntry
-extends HBoxContainer
+extends BoxContainer
+
+signal clicked
+
+var is_mouse_over := false
 
 func set_texture(texture: Texture2D) -> void:
-	$MonSprite.texture = texture
+	$Free/SpriteContainer/MonSprite.texture = texture
 
 func set_progress(progress: int) -> void:
 	assert(progress >= 0 && progress <= 100, "Progress must be a percentage from 0-100%")
-	$ProgressBar.value = progress
-	$MonSprite.modulate = Global.COLOR_BLACK if $ProgressBar.value != 100 else Global.COLOR_WHITE
+	$Free/ProgressBar.value = progress
+	$Free/SpriteContainer/MonSprite.modulate = Global.COLOR_BLACK if $Free/ProgressBar.value != 100 else Global.COLOR_WHITE
+
+func select():
+	$BackgroundUnselected.visible = false
+	$BackgroundSelected.visible = true
+
+func unselect():
+	$BackgroundUnselected.visible = true
+	$BackgroundSelected.visible = false
+
+func _input(event):
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT and is_mouse_over:
+		emit_signal("clicked", self)
+
+
+func _on_mouse_entered():
+	is_mouse_over = true
+
+
+func _on_mouse_exited():
+	is_mouse_over = false
--- ui/database/database_menu.gd
+++ ui/database/database_menu.gd
@@ -6,6 +6,7 @@ signal closed
 
 # MonType -> DatabaseEntry; maps montypes to their database entry scenes
 var _montype_to_entry_map := {}
+var active_entry = null
 
 const DATABASE_ENTRY_SCENE := preload("res://ui/database/database_entry.tscn")
 
@@ -17,9 +18,10 @@ func _ready() -> void:
 				continue
 			var entry: DatabaseEntry = DATABASE_ENTRY_SCENE.instantiate()
 			entry.set_texture(MonData.get_texture_for_mon(montype))
-			entry.set_progress(0)
+			entry.set_progress(50)
 			_montype_to_entry_map[montype] = entry
 			$DatabaseScroll/Database.add_child(entry)
+			entry.clicked.connect(_on_entry_clicked)
 
 func setup() -> void:
 	# update progress values for each mon
@@ -27,6 +29,12 @@ func setup() -> void:
 		if montype == MonData.MonType.NONE:
 			continue
 		_montype_to_entry_map[montype].set_progress(GameData.compilation_progress_per_mon[montype])
+
+func _on_entry_clicked(entry: DatabaseEntry) -> void:
+	if active_entry:
+		active_entry.unselect()
+	active_entry = entry
+	active_entry.select()
 
 func _on_exit_pressed() -> void:
 	emit_signal("closed")
