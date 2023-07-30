7a5d151b1f812dcf167b202178cb47d81354e6ce
bdunko <bdunko@gmail.com>
2023-07-30 15:41:04 -0400

    Fixed a narrowing conversion warning and add some asserts to camera

--- overworld/camera.gd
+++ overworld/camera.gd
@@ -2,7 +2,9 @@ extends Camera2D
 
 # Sets the limits of the camera to exactly fit the given tilemap
 func set_limits(map: Sprite2D):
-	self.limit_left = map.position.x
-	self.limit_right = map.position.x + map.texture.get_width()
-	self.limit_top = map.position.y
-	self.limit_bottom = map.position.y + map.texture.get_height()
+	assert(map.position.x == int(map.position.x), "Camera won't work unless map is aligned to an integer position.")
+	assert(map.position.y == int(map.position.y), "Camera won't work unless map is aligned to an integer position.")
+	self.limit_left = int(map.position.x)
+	self.limit_right = int(map.position.x) + map.texture.get_width()
+	self.limit_top = int(map.position.y)
+	self.limit_bottom = int(map.position.y) + map.texture.get_height()
