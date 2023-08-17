8b27ac38de0b2af32a4bf1828e086d122142bbb5
bdunko <bdunko@gmail.com>
2023-08-16 21:28:17 -0400

    Swap to new script interface

--- ui/script/script_menu.gd
+++ ui/script/script_menu.gd
@@ -1,71 +1,75 @@
 class_name UIScriptMenu
-extends VBoxContainer
+extends Node2D
 
 # emitted when this menu should be closed
 signal closed
 
-var monEdited: MonData.Mon
+var _active_file_tab = 1
+var _active_drawer_tab = 1
 
-const UI_SCRIPT_LINE_SCENE = preload("res://ui/script/script_line.tscn")
+const SCRIPT_BLOCK_SCENE = preload("res://ui/script/script_block.tscn")
 
-func setup(mon: MonData.Mon) -> void:
-	self.monEdited = mon
-	
-	$MenuBar/Left/MonNameLabel.text = mon.get_name()
-	
-	# clear previous data
-	for line in $Window/Lines.get_children():
-		line.queue_free()
-	
-	var script: ScriptData.MonScript = mon.get_monscript()
-	_import(script)
+func _ready():
+	# create blocks
+	for ifBlock in ScriptData.IF_BLOCK_LIST:
+		_create_and_add_block_to($BlockDrawer/Drawers/IfDrawer/BlockScroll/Blocks, ifBlock.type, ifBlock.name)
+	for doBlock in ScriptData.DO_BLOCK_LIST:
+		_create_and_add_block_to($BlockDrawer/Drawers/DoDrawer/BlockScroll/Blocks, doBlock.type, doBlock.name)
+	for toBlock in ScriptData.TO_BLOCK_LIST:
+		_create_and_add_block_to($BlockDrawer/Drawers/ToDrawer/BlockScroll/Blocks, toBlock.type, toBlock.name)
+	_update_drawer()
+	_update_file_tabs()
 
-func _add_new_line() -> UIScriptLine:
-	var line: UIScriptLine = UI_SCRIPT_LINE_SCENE.instantiate()
-	line.line_started.connect(_on_line_started)
-	line.line_deleted.connect(_on_line_deleted)
-	line.line_edited.connect(_on_line_edited)
-	$Window/Lines.add_child(line)
-	return line
-	
-func _on_line_started() -> void:
-	_add_new_line()
+func setup(mon: MonData.Mon):
+	pass
 
-func _on_line_deleted(line: UIScriptLine) -> void:
-	line.queue_free()
-	$Window/Lines.remove_child(line)
-	if $Window/Lines.get_child_count() == 0:
-		_add_new_line()
-	$MenuBar/Middle/Save.disabled = not line.is_valid()
+func _create_and_add_block_to(drawer: FlowContainer, block_type: ScriptData.Block.Type, block_name: String):
+	var block := SCRIPT_BLOCK_SCENE.instantiate()
+	block.set_data(block_type, block_name)
+	drawer.add_child(block)
 
-func _on_line_edited(line: UIScriptLine):
-	$MenuBar/Middle/Save.disabled = not line.is_valid()
+func _update_drawer():
+	_select_one_tab(_active_drawer_tab, $BlockDrawer/Tabs.get_children()) # update the tabs
+	var n := 1
+	for drawer in $BlockDrawer/Drawers.get_children():
+		drawer.visible = n == _active_drawer_tab
+		n += 1
 
-func _export_script_to_mon() -> void:
-	#insert header
-	var script_str := ScriptData.SCRIPT_START
-	
-	#insert lines
-	for line in $Window/Lines.get_children():
-		script_str += ScriptData.LINE_DELIMITER
-		script_str += line.export()
-	
-	#insert footer
-	script_str += ScriptData.LINE_DELIMITER + ScriptData.SCRIPT_END
-	
-	#parse as a script
-	var script: ScriptData.MonScript = ScriptData.MonScript.new(script_str)
-	
-	monEdited.set_monscript(script)
+func _update_file_tabs():
+	_select_one_tab(_active_file_tab, $FileTabs.get_children())
 
-func _import(script: ScriptData.MonScript) -> void:
-	for line in script.lines:
-		var new_line: UIScriptLine = _add_new_line()
-		new_line.import(line)
+func _select_one_tab(active_tab: int, tab_elements: Array):
+	var n = 1
+	for tab in tab_elements:
+		tab.select() if n == active_tab else tab.unselect()
+		n += 1
 
-func _on_save_pressed() -> void:
-	_export_script_to_mon()
+func _on_file_tab_1_clicked():
+	_active_file_tab = 1
+	_update_file_tabs()
+
+func _on_file_tab_2_clicked():
+	_active_file_tab = 2
+	_update_file_tabs()
+
+func _on_file_tab_3_clicked():
+	_active_file_tab = 3
+	_update_file_tabs()
+
+func _on_clear_button_pressed():
+	print("CLEAR")
+
+func _on_x_button_pressed():
 	emit_signal("closed")
 
-func _on_exit_pressed() -> void:
-	emit_signal("closed")
+func _on_if_tab_clicked():
+	_active_drawer_tab = 1
+	_update_drawer()
+
+func _on_do_tab_clicked():
+	_active_drawer_tab = 2
+	_update_drawer()
+
+func _on_to_tab_clicked():
+	_active_drawer_tab = 3
+	_update_drawer()
---
+++ ui/scriptv0/script_block.gd
@@ -0,0 +1,118 @@
+extends Control
+
+signal text_changed
+
+var options := ["ERROR"]
+var type: ScriptData.Block.Type
+
+# whether this block was valid at one point in time or not
+var was_once_valid := false
+
+func assign_type(block_type: ScriptData.Block.Type) -> void:
+	type = block_type
+	
+	# get the list of blocks to be added as options
+	var block_list := []
+	match type:
+		ScriptData.Block.Type.IF:
+			block_list = ScriptData.IF_BLOCK_LIST 
+		ScriptData.Block.Type.DO:
+			block_list = ScriptData.DO_BLOCK_LIST
+		ScriptData.Block.Type.TO:
+			block_list = ScriptData.TO_BLOCK_LIST
+		_:
+			assert(false, "No match found for block_type!")
+	
+	options.clear()
+	
+	# add each block's name to options
+	for block in block_list:
+		assert(block.type == type, "Types don't match!")
+		options.append(block.name)
+	assert(options.size() != 0, "No options for script dropdown!")
+	
+	_update_intellisense()
+
+func _ready() -> void:
+	$CanvasLayer/Intellisense.visible = false
+
+func import(block: ScriptData.Block) -> void:
+	$TextEdit.text = block.as_string()
+	$TextEdit.emit_signal("text_changed")
+	assert(is_valid(), "Imported block is invalid?")
+
+func export() -> String:
+	return $TextEdit.text
+
+func _on_intellisense_item_clicked(index: int, _at_position: Vector2, _mouse_button_index: int) -> void:
+	var selected_suggestion = $CanvasLayer/Intellisense.get_item_text(index)
+	$TextEdit.text = selected_suggestion
+	$TextEdit.emit_signal("text_changed")
+	$CanvasLayer/Intellisense.visible = false
+
+func _has_prefix(prefix: String, s: String) -> bool:
+	if prefix.length() > s.length():
+		return false
+	for i in range(0, prefix.length()):
+		if prefix[i] != s[i]:
+			return false
+	return true
+
+const EXCLUDED_KEYS := [KEY_SPACE, KEY_TAB, KEY_ENTER]
+func _input(event: InputEvent) -> void:
+	# block space and enter inputs to TextEdit
+	if $TextEdit.has_focus():
+		if event is InputEventKey and event.is_pressed():
+			if event.keycode in EXCLUDED_KEYS:
+				get_viewport().set_input_as_handled()
+
+func _on_text_changed() -> void:
+	emit_signal("text_changed")
+	_update_intellisense()
+	if is_valid():
+		was_once_valid = true
+		$TextEdit.add_theme_color_override("font_color", Global.COLOR_GREEN)
+	else:
+		# if we were valid in the past, make it red now
+		if was_once_valid:
+			$TextEdit.add_theme_color_override("font_color", Global.COLOR_RED)
+		else:
+			$TextEdit.remove_theme_color_override("font_color")
+
+func _update_intellisense() -> void:
+	if(get_parent() != null): # horrible terrible hack
+		var translate = get_parent().position + self.position + Vector2(0, 33)
+		$CanvasLayer.transform = Transform2D(Vector2(1, 0), Vector2(0, 1), translate)
+	
+	$CanvasLayer/Intellisense.clear()
+	for option in options:
+		if _has_prefix($TextEdit.text.to_lower(), option.to_lower()):
+			$CanvasLayer/Intellisense.add_item(option)
+	
+	if $CanvasLayer/Intellisense.item_count != 0:
+			$CanvasLayer/Intellisense.visible = true
+	
+	if $TextEdit.text.length() == 0 or is_valid():
+		$CanvasLayer/Intellisense.visible = false
+
+func is_valid() -> bool:
+	for option in options:
+		if option.to_lower() == $TextEdit.text.to_lower():
+			$TextEdit.text = option
+			$TextEdit.set_caret_column($TextEdit.text.length())
+			return true
+	return false
+
+func next_block_type() -> ScriptData.Block.Type:
+	var block = ScriptData.get_block_by_name($TextEdit.text)
+	if block == null:
+		return ScriptData.Block.Type.NONE
+	return block.next_block_type
+
+func _on_text_edit_focus_exited():
+	# don't close intellisense if we're clicking on it... otherwise close it
+	if not $CanvasLayer/Intellisense.get_rect().has_point(get_local_mouse_position()):
+		$CanvasLayer/Intellisense.visible = false
+
+func _on_text_edit_focus_entered():
+	_update_intellisense()
