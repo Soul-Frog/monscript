6efa78ccc607093f149469f0ed8bd7882500c96d
bdunko <bdunko@gmail.com>
2023-08-19 22:46:19 -0400

    Fixed block positioning in drawers

--- ui/script/script_menu.gd
+++ ui/script/script_menu.gd
@@ -16,25 +16,47 @@ var held_block = null
 const SCRIPT_BLOCK_SCENE = preload("res://ui/script/script_block.tscn")
 const SCRIPT_LINE_SCENE = preload("res://ui/script/script_line.tscn")
 
+@onready var SCRIPT_SCROLL = $ScriptScroll
 @onready var SCRIPT_LINES = $ScriptScroll/Script/ScriptLines
 
+@onready var BLOCK_TABS = $BlockDrawer/Tabs
+@onready var BLOCK_DRAWERS = $BlockDrawer/Drawers
+@onready var IF_DRAWER = $BlockDrawer/Drawers/IfDrawer/BlockScroll/Margins/Blocks
+@onready var DO_DRAWER = $BlockDrawer/Drawers/DoDrawer/BlockScroll/Margins/Blocks
+@onready var TO_DRAWER = $BlockDrawer/Drawers/ToDrawer/BlockScroll/Margins/Blocks
+
+@onready var DISCARD_ZONE = $DiscardBlockArea/Shape
+
+@onready var FILE_TABS = $FileTabs
+
+
 func _ready() -> void:
+	assert(SCRIPT_SCROLL != null)
+	assert(SCRIPT_LINES != null)
+	assert(BLOCK_TABS != null)
+	assert(BLOCK_DRAWERS != null)
+	assert(IF_DRAWER != null)
+	assert(DO_DRAWER != null)
+	assert(TO_DRAWER != null)
+	assert(DISCARD_ZONE != null)
+	assert(FILE_TABS != null)
+	
 	# create blocks
 	for ifBlock in ScriptData.IF_BLOCK_LIST:
-		_create_and_add_block_to($BlockDrawer/Drawers/IfDrawer/BlockScroll/Blocks, ifBlock.type, ifBlock.name)
+		_create_and_add_block_to(IF_DRAWER, ifBlock.type, ifBlock.name)
 	for doBlock in ScriptData.DO_BLOCK_LIST:
-		_create_and_add_block_to($BlockDrawer/Drawers/DoDrawer/BlockScroll/Blocks, doBlock.type, doBlock.name)
+		_create_and_add_block_to(DO_DRAWER, doBlock.type, doBlock.name)
 	for toBlock in ScriptData.TO_BLOCK_LIST:
-		_create_and_add_block_to($BlockDrawer/Drawers/ToDrawer/BlockScroll/Blocks, toBlock.type, toBlock.name)
+		_create_and_add_block_to(TO_DRAWER, toBlock.type, toBlock.name)
 	_update_drawer()
 	_update_file_tabs()
 	
 	# when the scrollbar size changes, move the scrollbar down
-	$ScriptScroll.get_v_scroll_bar().changed.connect(_move_scroll_to_bottom)
+	SCRIPT_SCROLL.get_v_scroll_bar().changed.connect(_move_scroll_to_bottom)
 
 func setup(mon: MonData.Mon) -> void:
 	held_block = null
-	$DiscardBlockArea/Shape.disabled = true
+	DISCARD_ZONE.disabled = true
 
 func _create_block(block_type: ScriptData.Block.Type, block_name: String) -> UIScriptBlock:
 	var block := SCRIPT_BLOCK_SCENE.instantiate()
@@ -48,16 +70,16 @@ func _create_and_add_block_to(drawer: FlowContainer, block_type: ScriptData.Bloc
 	block.visible = false
 
 func _update_drawer() -> void:
-	_select_one_tab(_active_drawer_tab, $BlockDrawer/Tabs.get_children()) # update the tabs
+	_select_one_tab(_active_drawer_tab, BLOCK_TABS.get_children()) # update the tabs
 	var n := 1
-	for drawer in $BlockDrawer/Drawers.get_children():
+	for drawer in BLOCK_DRAWERS.get_children():
 		drawer.visible = n == _active_drawer_tab
 		for block in drawer.find_child("BlockScroll").find_child("Blocks").get_children():
 			block.visible = n == _active_drawer_tab
 		n += 1
 
 func _update_file_tabs() -> void:
-	_select_one_tab(_active_file_tab, $FileTabs.get_children())
+	_select_one_tab(_active_file_tab, FILE_TABS.get_children())
 
 func _select_one_tab(active_tab: int, tab_elements: Array) -> void:
 	var n = 1
@@ -120,7 +142,7 @@ func _pickup_held_block(new_held_block: UIScriptBlock) -> void:
 	new_held_block.position = Vector2(-100, -100)
 	new_held_block.z_index = 100 # draw this on top of everything
 	call_deferred("_set_initial_block_position_and_notify_lines") # wait a frame to set this so size can update
-	$DiscardBlockArea/Shape.disabled = false
+	DISCARD_ZONE.disabled = false
 func _set_initial_block_position_and_notify_lines() -> void:
 	if(held_block != null):
 		held_block.position = Global.centered_position(held_block, get_viewport().get_mouse_position())
@@ -131,7 +153,7 @@ func _discard_held_block(delete: bool) -> void:
 	if delete:
 		held_block.queue_free()
 	held_block = null
-	$DiscardBlockArea/Shape.disabled = true
+	DISCARD_ZONE.disabled = true
 	for script_line in SCRIPT_LINES.get_children():
 		script_line.notify_held_block(null)
 
@@ -139,17 +161,17 @@ func _on_new_line_button_pressed() -> void:
 	var newline = SCRIPT_LINE_SCENE.instantiate()
 	newline.deleted.connect(_on_line_deleted)
 	newline.clicked_dropzone.connect(_on_dropzone_clicked)
-	$ScriptScroll/Script/ScriptLines.add_child(newline)
+	SCRIPT_LINES.add_child(newline)
 	_update_line_numbers()
 	
 func _move_scroll_to_bottom() -> void:
 	# if the scrollbar has grown, we just added a new line
 	# move down to the next line.
-	if _max_scroll < $ScriptScroll.get_v_scroll_bar().max_value:
-		$ScriptScroll.scroll_vertical = $ScriptScroll.get_v_scroll_bar().max_value
+	if _max_scroll < SCRIPT_SCROLL.get_v_scroll_bar().max_value:
+		SCRIPT_SCROLL.scroll_vertical = SCRIPT_SCROLL.get_v_scroll_bar().max_value
 		
 	# either way, update our known scroll size.
-	_max_scroll = $ScriptScroll.get_v_scroll_bar().max_value
+	_max_scroll = SCRIPT_SCROLL.get_v_scroll_bar().max_value
 
 func _on_line_deleted(deleted_line: UIScriptLine) -> void:
 	deleted_line.get_parent().remove_child(deleted_line)
@@ -158,7 +180,7 @@ func _on_line_deleted(deleted_line: UIScriptLine) -> void:
 
 func _update_line_numbers() -> void:
 	var n := 1
-	for script_line in $ScriptScroll/Script/ScriptLines.get_children():
+	for script_line in SCRIPT_LINES.get_children():
 		script_line.set_line_number(n)
 		n += 1
 
