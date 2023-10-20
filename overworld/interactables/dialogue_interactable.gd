e9884ac9d88b7dc24d2e86d3594b6fdef0171e6f
bdunko <bdunko@gmail.com>
2023-10-20 00:03:49 -0400

    Improved water switch; fixed broken blue hat

--- overworld/interactables/dialogue_interactable.gd
+++ overworld/interactables/dialogue_interactable.gd
@@ -4,11 +4,8 @@ extends Interactable
 @export var dialogue_start: String = "test"
 
 func _onInteract():
-	# disable player movement
-	_INTERACTION_AREA.get_overlapping_bodies()[0].disable_movement()
-	
-	# play the dialogue
-	await Dialogue.play(dialogue_resource, dialogue_start)
-	
-	# enable player movement
-	_INTERACTION_AREA.get_overlapping_bodies()[0].enable_movement()
+	# don't open a new dialogue if we're already talking
+	if not Dialogue.is_dialogue_active():
+		_INTERACTION_AREA.get_overlapping_bodies()[0].disable_movement() # disable player movement
+		await Dialogue.play(dialogue_resource, dialogue_start) # play the dialogue
+		_INTERACTION_AREA.get_overlapping_bodies()[0].enable_movement() # enable player movement
--- overworld/interactables/water_switch_interactable.gd
+++ overworld/interactables/water_switch_interactable.gd
@@ -1,6 +1,10 @@
 extends Interactable
 
+#make this global l8r and also do some nice signal trix
 var water_up = true
+
+#in ready, connect to a switched signal
+#
 
 func _onInteract():
 	water_up = not water_up
