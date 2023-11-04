37daefa76ad867f96908e0ae3fbdef746620e1e3
bdunko <bdunko@gmail.com>
2023-11-04 16:21:36 -0400

    Improved suck zone behavior

--- overworld/areas/coolant_cave/suck_zone.gd
+++ overworld/areas/coolant_cave/suck_zone.gd
@@ -2,7 +2,7 @@
 class_name SuckZone
 extends Area2D
 
-@export var SUCTION_STRENGTH: float = 50 #how quickly the whirlpool pulls player to center at maximum
+@export var SUCTION_STRENGTH: float = 100 #how quickly the whirlpool pulls player to center at maximum
 
 @onready var _CIRCLE = $SuckCircle
 @onready var _CENTER: Vector2 = _CIRCLE.position
@@ -26,11 +26,16 @@ func _process(delta):
 		
 		var x_direction = -1 if magic_position.x > _CENTER.x else 1 #pull left or right?
 		var y_direction = -1 if magic_position.y > _CENTER.y else 1 #pull up or down?
-		var force = (1-(dist/_MAX_RADIUS)) * SUCTION_STRENGTH #calculate force based on distance to center
+		var total_force = (1-(dist/_MAX_RADIUS)) * SUCTION_STRENGTH #calculate force based on distance to center
+		
+		# we want to look like we're smoothly sliding to the center, so if we are closer in one dimension, 
+		# apply less to that dimension
+		var x_force_mult = x_dist / dist
+		var y_force_mult = y_dist / dist
 		
 		# if within a reasonable range to the center (3 pixels); don't apply force to avoid jittering
-		var x_force = 0 if x_dist <= 5 else force * x_direction
-		var y_force = 0 if y_dist <= 5 else force * y_direction
+		var x_force = 0 if x_dist <= 5 else total_force * x_force_mult * x_direction
+		var y_force = 0 if y_dist <= 5 else total_force * y_force_mult * y_direction
 		
 		# apply force to player body
 		body.apply_velocity(Vector2(x_force, y_force))
--- overworld/components/foreground_element.gd
+++ overworld/components/foreground_element.gd
@@ -3,10 +3,14 @@ extends Sprite2D
 
 @onready var _FADE = $FadeDecorator
 
+# TODO - figure out if I really want these fades or not...
+
 func _on_fade_zone_body_entered(body):
+	return
 	if body is Player:
 		_FADE.fade_out()
 
 func _on_fade_zone_body_exited(body):
+	return
 	if body is Player:
 		_FADE.fade_in()
