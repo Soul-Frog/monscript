c5f9ef034b69deb613234ce5f5f67e60973eedfe
bdunko <bdunko@gmail.com>
2023-11-28 23:30:58 -0500

    Added more world layers for future maps; added mons to cave2 and cave3

--- overworld/components/elevation_transition_zone.gd
+++ overworld/components/elevation_transition_zone.gd
@@ -1,17 +1,31 @@
 extends Area2D
 
 enum Type {
-	ToLower, ToUpper
+	ToTop, ToUpper, ToMiddle, ToLower, ToBottom, None
 }
 
-@export var transition_type = Type.ToLower
+@export var transition_type = Type.None
+
+func _ready():
+	assert(transition_type != Type.None)
+
+# set the mask values such that exactly 'mask' is true and the rest are false
+# basically, we can only be on one layer at a time
+func _set_one_mask(body, mask):
+	body.set_collision_mask_value(5, 5 == mask)
+	body.set_collision_mask_value(6, 6 == mask)
+	body.set_collision_mask_value(7, 7 == mask)
+	body.set_collision_mask_value(8, 8 == mask)
+	body.set_collision_mask_value(9, 9 == mask)
 
 func _on_body_entered(body):
-	if transition_type == Type.ToLower:
-		# change from upper to lower level
-		body.set_collision_mask_value(5, false)
-		body.set_collision_mask_value(6, true)
-	else:
-		# change from lower to upper level
-		body.set_collision_mask_value(6, false)
-		body.set_collision_mask_value(5, true)
+	if transition_type == Type.ToTop:
+		_set_one_mask(body, 5)
+	elif transition_type == Type.ToUpper:
+		_set_one_mask(body, 6)
+	elif transition_type == Type.ToMiddle:
+		_set_one_mask(body, 7)
+	elif transition_type == Type.ToLower:
+		_set_one_mask(body, 8)
+	elif transition_type == Type.ToBottom:
+		_set_one_mask(body, 9)
