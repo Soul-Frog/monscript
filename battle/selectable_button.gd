c002c999cb0c551422416c1a791be79b8d3b7165
bdunko <bdunko@gmail.com>
2024-01-28 13:35:14 -0500

    Fixed player shader; fixed bunton disabled bug; cleanup

--- battle/selectable_button.gd
+++ battle/selectable_button.gd
@@ -21,8 +21,6 @@ func _ready():
 	assert(texture_unselected)
 	assert(texture_selected)
 	
-	disabled = true
-	
 	self.pressed.connect(_on_pressed)
 	_default_state = selected
 	_update_textures()
--- ui/customization/customization_selector.gd
+++ ui/customization/customization_selector.gd
@@ -1,9 +1,5 @@
 extends Node2D
 
-# - All buttons are clickable already but we need to detect when it was clicked
-# - One can be active at a time and show it visually, and report which color has been selected (getter)
-# - Detect what button was clicked and print to console
-
 enum CustomizationPart {
 	HAIR,
 	EYES,
@@ -35,7 +31,6 @@ func _on_button_pressed(child: TextureButton) -> void:
 	active_button.disabled = false
 	active_button = child
 	active_button.z_index = 0
-	var color = child.name
 	$Indicator.position = active_button.position
 	$Indicator.visible = true
 	active_button.disabled = true
