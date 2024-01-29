b1d0c5191b41da741358d487dd4fdc2d6e36f617
bdunko <bdunko@gmail.com>
2024-01-29 00:36:38 -0500

    Added disabled state for customization submit button; disable submit button when name field is empty

--- ui/customization/customization.gd
+++ ui/customization/customization.gd
@@ -7,16 +7,21 @@ signal customization_complete
 @onready var BUNTON = $CharacterCustomizationPanel/Bunton
 @onready var PLAYER_PREVIEW = $CharacterCustomizationPanel/PlayerOverhead
 @onready var NAME_INPUT = $CharacterCustomizationPanel/NameInput
+@onready var SUBMIT_BUTTON = $CharacterCustomizationPanel/SubmitButton
 
 func _ready():
 	assert(BUNTON)
 	assert(PLAYER_PREVIEW)
 	assert(NAME_INPUT)
+	assert(PANEL)
+	assert(SUBMIT_BUTTON)
+	SUBMIT_BUTTON.disabled = true
 	PLAYER_PREVIEW.disable_movement()
 
 func _input(event: InputEvent):
 	if event is InputEventKey:
 		NAME_INPUT.grab_focus()
+	SUBMIT_BUTTON.disabled = NAME_INPUT.text.length() == 0
 
 func _on_bunton_state_changed():
 	GameData.set_var(GameData.BUN_CUSTOMIZATION, BUNTON.selected)
