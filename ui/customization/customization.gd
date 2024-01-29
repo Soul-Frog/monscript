02fbcea6e1a9e417c1eab5aef64528651378f550
Emaan Rana <87550510+emaanr@users.noreply.github.com>
2024-01-28 20:01:45 -0500

    Bunton set up, also added a signal for Submit Button

--- ui/customization/customization.gd
+++ ui/customization/customization.gd
@@ -2,11 +2,27 @@ class_name Customization
 
 extends Node2D
 
+signal customization_complete
+
+@onready var BUNTON = $CharacterCustomizationScreen/Bunton
+@onready var PLAYER_PREVIEW = $CharacterCustomizationScreen/PlayerOverhead
+@onready var NAME_INPUT = $CharacterCustomizationScreen/NameInput
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$CharacterCustomizationScreen/PlayerOverhead.disable_movement()
+	assert(BUNTON)
+	assert(PLAYER_PREVIEW)
+	assert(NAME_INPUT)
+	PLAYER_PREVIEW.disable_movement()
+
+func _input(event: InputEvent):
+	if event is InputEventKey:
+		NAME_INPUT.grab_focus()
+
+func _on_bunton_state_changed():
+	GameData.set_var(GameData.BUN_CUSTOMIZATION, BUNTON.selected)
+	Events.emit_signal("update_player_sprite")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_submit_button_pressed():
+	emit_signal("customization_complete")
