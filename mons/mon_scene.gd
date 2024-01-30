e2f11d6182c5e669ab62231810aec379231c5892
bdunko <bdunko@gmail.com>
2024-01-29 23:13:04 -0500

    Updated leviathan sprite; added red hat

--- mons/mon_scene.gd
+++ mons/mon_scene.gd
@@ -60,7 +60,7 @@ func move_to_point(point: Vector2) -> void:
 	target_point = point
 
 func face_left() -> void:
-	$Sprite.flip_h = true
+	SPRITE.flip_h = true
 
 func face_right() -> void:
-	$Sprite.flip_h = false
+	SPRITE.flip_h = false
